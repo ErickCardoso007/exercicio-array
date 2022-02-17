3c231938191abda40e0fb3aa071a02eb5eeadfb2
Emilly marques <emillymarques96622@gmail.com>
2022-02-17 14:09:02 -0300

    tarefa media de array concluída

--- array.js
+++ array.js
@@ -1,3 +1,19 @@
-const nota = [
-    9, 9.4, 7.6, 5.5, 0.6, 7, 1.5, 4.5, 4, 3
-]
+function calculos() {
+    const nota = [
+        9, 9.4, 7.6, 5.5, 0.6, 7, 1.5, 4.5, 4, 3
+    ]
+    const divisao = nota.length
+    const somaMedia = nota.reduce(
+        (valor1, valor2) => valor1 + valor2
+    )
+    const mediaDaNota = somaMedia / divisao
+
+    if (mediaDaNota >= 6) {
+        return "aprovadoo. Parabéns por essa conquista!"
+    }
+
+
+    return "reprovado. Não teve sortee!"
+
+}
+console.log(calculos())

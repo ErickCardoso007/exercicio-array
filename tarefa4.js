8b0779db4c9de67a1ef61e18d469527223f8ef29
willian <willian.chris.89@gmail.com>
2022-02-17 15:48:06 -0300

    tarefa 4 em andamento

---
+++ tarefa4.js
@@ -0,0 +1,21 @@
+
+
+const valorInicial = 10
+const valorFinal = 30
+for (valorInicial; valorInicial < valorFinal; valorInicial++) {
+    console.log(valorInicial)
+}
+
+
+
+
+/*const numero = [
+    9, 9.4, 7.6, 5.5, 0.6, 7, 1.5, 4.5, 4, 3
+]
+if (numero / 2 = 0) {
+    return "par"
+}
+if (numero / 2 = 1) {
+    return "impar"
+}
+alert(total);*/

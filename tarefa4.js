43714c2ae7639a676f9984375bbea7b2468fb5da
willian <willian.chris.89@gmail.com>
2022-02-21 10:49:35 -0300

    retornando os numeros impares do exercicio 4

--- tarefa4.js
+++ tarefa4.js
@@ -1,14 +1,25 @@
 
 
-const valorInicial = 10
+let valorInicial = 11
 const valorFinal = 30
-for (valorInicial; valorInicial < valorFinal; valorInicial++) {
+for (valorInicial; valorInicial < valorFinal; valorInicial += 2) {
     console.log(valorInicial)
 }
 
 
 
+/*let valorInicial = 10  
 
+for (let i = 0; i < 30; i += 1)//fazendo a conversão de impar 
+{
+    valorInicial += i
+    console.log(valorInicial)
+}
+
+//11, 13, 15, 17, 19, 21, 23, 25, 27, 29
+
+//let somaImpar = soma + soma
+//console.log(somaImpar)
 /*const numero = [
     9, 9.4, 7.6, 5.5, 0.6, 7, 1.5, 4.5, 4, 3
 ]

9bbe57919eb623c9a6ab7609d01c3a894f9225bb
Emilly marques <emillymarques96622@gmail.com>
2022-02-22 14:57:45 -0300

    tarefa10 finalizada sem metodo

---
+++ tarefa10.js
@@ -0,0 +1,13 @@
+function juntaArray() {
+    const num = [1, 2, 3, 4, 5, 6]
+    const nam = [7, 8, 9, 10, 11, 12, 13]
+    const concatena = [num + "," + nam]
+    console.log(concatena)
+}
+
+juntaArray()
+
+
+
+/*10) 
+Criar uma função que junte dois arrays e retorno o resultado como um novo array  */

1a26265834051619f8b739f6d0cd67e416372750
Emilly marques <emillymarques96622@gmail.com>
2022-02-23 10:51:19 -0300

    tarefa 11 em andamento

--- tarefa11.js
+++ tarefa11.js
@@ -1,21 +1,61 @@
-function numPrimo(numero) {
-    /* const calculo = numero / numero
-     const primo = numero / 1*/
+function numPrimo(num) {
 
-    let contador = 0
-    for (let i = 1; i <= numero; i++) {
-        if (numero % i == 0) contador++;
-        return true
+    var numeroPrimoInicial = 1
+
+    while (numeroPrimoInicial <= num) {
+
+        if (numeroPrimoInicial / numeroPrimoInicial) {
+            numeroPrimoInicial = numeroPrimoInicial + 1
+            return true
+        }
+        else { return false }
     }
-    return false
 }
-console.log(numPrimo(4))
+console.log(numPrimo(16))
+
+/*
+    for (let i = 2; i <= num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+        return num > 1;
+    }
+*/
+
+/* const divisao = numero / numero
+const verificaPrimo = numero / divisao
+//console.log(verificaPrimo)
+if (verificaPrimo % ) {
+    return true
+}
+else () {
+
+    return false
+}*/
+
 
 
 /*11) explicacação:
 Criar uma função que retorne um valor true or false informando se o número enviado como 
 parâmetro é um número primo.
-numeros primos : 3,5,7,11,13,17
+Um número primo é um número que só pode ser dividido por 1 e por ele mesmo.
+Por exemplo: 3,5,7,11,13,17
+
+function primo(){
+  
+  var divisores=0;
+
+  for(var count=1 ; count<=numero ; count++)
+   if(numero % count == 0)
+          divisores++;
+  
+  if(divisores==2)
+        return true;
+  else
+        return false;
+}
+
+
 
 for (let i = 2; i < num; i++)     
 if (num % i === 0) {       return false;     }   return num > 1;

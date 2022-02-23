2c44e892f218a6ef3935a5fc8717322d70941d9f
Emilly marques <emillymarques96622@gmail.com>
2022-02-23 14:24:59 -0300

    tarefa 13 finalizada

---
+++ tarefa13.js
@@ -0,0 +1,10 @@
+function calculaPorcentagem(numero) {
+    const divide = numero / 100
+    return `a porcentagem de ${numero} é: ${divide}`
+}
+console.log(calculaPorcentagem(500))
+
+
+
+/*Criar uma função para calcular a porcentagem de um número 
+*/

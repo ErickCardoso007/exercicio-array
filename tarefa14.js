1085be39d517779b9479b6c946340b5e116d1435
Emilly marques <emillymarques96622@gmail.com>
2022-02-24 09:43:59 -0300

    tarefa 14 finalizada

---
+++ tarefa14.js
@@ -0,0 +1,11 @@
+function fatorial(num) {
+    const multiplica = num * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+    return `o fatorial de ${num} é ${multiplica}`
+}
+console.log(fatorial(10))
+
+/*
+14)
+Calculado o fatorial de 10
+O fatorial de um número é calculado pela multiplicação desse número por todos os seus antecessores até chegar ao número 1.
+*/
